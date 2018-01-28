8d1067d4ae917fc8a620d85a947c8de5c96f60f9
Michael Hayden <mwhayden1975@gmail.com>
2018-01-28 09:26:56 -0500

    Updated service-worker.js to ECMA6

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,56 +1,74 @@
 // service worker
 
 // add data to cache
-var dataCacheName = 'data-cache-v1';
-var cacheName = 'data-cache';
-var files = [
-  '/',
-  '/manifest.json',
-  'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css',
-  '/js/app.js'
-];
+var log = console.log.bind(console),
+	err = console.error.bind(console),
+	cacheData = 'data-cache-v1',
+	cacheName = 'data-cache',
+	files = [
+  		'/',
+  		'/manifest.json',
+  		'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css',
+  		'/js/app.js'
+	];
 
 // service worker installation
-self.addEventListener('install', function(e) {
-  console.log('[ServiceWorker] Install');
-  e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      console.log('[ServiceWorker] Caching app shell');
-      return cache.addAll(files);
-    })
-  );
+self.addEventListener('install', (e) => { 
+	log('[Service Worker] Install'); 
+	e.waitUntil(
+		caches.open(cacheName).then((cache) => {
+  			log('[Service Worker] Caching app shell');
+  			return cache.addAll(files);
+		})
+	);
 });
 
 // service worker activation
-self.addEventListener('activate', function(e) {
-  console.log('[ServiceWorker] Activate');
-  e.waitUntil(
-    caches.keys().then(function(keyList) {
-      return Promise.all(keyList.map(function(key) {
-        if (key !== cacheName && key !== dataCacheName) {
-          console.log('[ServiceWorker] Removing old cache', key);
-          return caches.delete(key);
-        }
-      }));
-    })
-  );
-  return self.clients.claim();
+self.addEventListener('activate',(e) => { 
+	log('[Service Worker] Activate');
+  	e.waitUntil(
+    	caches.keys().then((keyList) => {
+      		return Promise.all(keyList.map((key) => {
+        		if (key !== cacheName && key !== cacheData) {
+          			log('[Service Worker] Removing old cache', key);
+          			return caches.delete(key);
+        		}
+      		}));
+    	})
+  	);
+  	return self.clients.claim();
 });
 
-// service worker fetch operation
-self.addEventListener('fetch', function(event) {
-  console.log('[ServiceWorker] Fetch ' + event.request, event);
-  event.respondWith(
-    caches.match(event.request).then(function(res) {
-      return res || fetch(event.request).then(function(response) {
-        caches.open(cacheName).then(function(cache) {
-          console.log('[ServiceWorker] Caches open match', response);
-          cache.put(event.request, response.clone());
-        });
-        return response;
-      });
-    }).catch(function(e) {
-      console.log('[ServiceWorker] Fetch catch error', e);
-    })
-  );
+// service worker fetch
+self.addEventListener('fetch', (e) => { 
+	log('[Service Worker] Fetch', e);
+  	e.respondWith(
+    	caches.match(e.request).then((res) => {
+      		return res || fetch(e.request).then((response) => {
+        		caches.open(cacheName).then((cache) => {
+          			log('[Service Worker] Caches open match', response);
+          			cache.put(e.request, response.clone());
+        		});
+        		return response;
+      		});
+    	}).catch((e) => {
+      		err('[Service Worker] Fetch catch error', e);
+      		// add cache match fallback
+    	})
+  	);
+});
+
+// service worker push notification
+self.addEventListener('push', (e) => {
+	log('[Service Worker] Push Notification')
+	e.waitUntil(
+		fetch('/pushdata').then((response) => {
+  			return response.json();
+		}).then((data) => {
+			log('[Service Worker] Push data', data);
+		}).catch((e) => {
+			err('[Service Worker] Push catch error',e);
+
+		})
+	);
 });
