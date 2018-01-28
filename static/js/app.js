6bfebb1d4225c57218a6adb17252672c7f685b89
Michael Hayden <mwhayden1975@gmail.com>
2018-01-28 13:34:12 -0500

    Updated app.js, added push notifications, geolocation events

--- static/js/app.js
+++ static/js/app.js
@@ -4,9 +4,9 @@ window.addEventListener('beforeinstallprompt', function(e) {
   e.userChoice.then(function(choiceResult) {
       console.log('User choice outcome', choiceResult);
     if (choiceResult.outcome == 'dismissed')
-      console.log('User cancelled home screen install');
+      console.log('User cancelled home screen installation.');
     else
-      console.log('User added to home screen');
+      console.log('User added app to home screen.');
   });
 });
 
@@ -26,16 +26,70 @@ window.addEventListener('beforeinstallprompt', function(e) {
 
 	  }).catch(function(error) {
 		// registration failed
-		console.error('Registration failed with ' + error);
+		console.error('Registration failed.', error);
 		
 		// Firefox Nightly
 		// Go to about:config and set dom.serviceWorkers.enabled to true; restart browser.
 		// Chrome Canary
-        // Go to chrome://flags and turn on experimental-web-platform-features; restart browser (note that some features are now enabled by default in Chrome.)
+		// Go to chrome://flags and turn on experimental-web-platform-features; restart browser (note that some features are now enabled by default in Chrome.)
 		// Opera
 		// Go to opera://flags and enable Support for ServiceWorker; restart browser.
 		// Microsoft Edge
 		// Go to about://flags and tick Enable service workers; restart browser.
 	  });
   }
+  
+  // Request geo location data
+  if ('geolocation' in navigator) {
+    var watchID, 
+        target = { latitude : 0, longitude: 0 }, 
+        options = { enableHighAccuracy: false, timeout: 5000, maximumAge: 0 };
+    
+    // ask permission
+    watchID = navigator.geolocation.watchPosition(geoSuccess, geoError, options);
+    
+    // success
+    function geoSuccess(position) {
+      console.log('Position updated.', position);
+      if (target.latitude === position.coords.latitude 
+      &&  target.longitude === position.coords.longitude) {
+         console.warn('You reached the target.');
+         //navigator.geolocation.clearWatch(id);
+      }
+    }
+    
+    // failure 
+    function geoError(error) {
+      switch(error.code) {
+        case error.PERMISSION_DENIED:
+          console.warn("User denied the request for Geolocation.", error)
+          break;
+        case error.POSITION_UNAVAILABLE:
+          console.warn("Location information is unavailable.", error)
+          break;
+        case error.TIMEOUT:
+          console.warn("The request to get user location timed out.", error)
+          break;
+        case error.UNKNOWN_ERROR:
+          console.warn("An unknown error occurred.", error)
+          break;
+      }
+    }
+  }
+
+  // Request permission to send notifications
+  if ('requestPermission' in Notification) {
+	Notification.requestPermission().then(function() {
+		return navigator.serviceWorker.ready;
+	}).then(function(sw) {
+		// Tell it to subscribe with the push server
+		return sw.pushManager.subscribe({userVisibleOnly: true});
+	}).then(function(subscription) {
+		// Now we have a subscription!
+		console.log('Subscribed to push notifications.', subscription)
+	}).catch(function(error) {
+		console.error('Push subscription error.', error);
+	});
+  }  
+  
 })();
