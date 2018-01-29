77154038303ae72a8f7464058a3c293fc5944697
Michael Hayden <mwhayden1975@gmail.com>
2018-01-28 19:59:39 -0500

    Fixed indentations

--- static/service-worker.js
+++ static/service-worker.js
@@ -3,16 +3,20 @@
 // add data to cache
 var log = console.log.bind(console),
     err = console.error.bind(console),
+    
+    // create cache keys
     cacheData = 'data-cache-v1',
     cacheName = 'data-cache',
+    
+    // add files to cache
     files = [
-  		'/',
-  		'/manifest.json',
-  		'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css',
-        'https://code.jquery.com/jquery-3.2.1.slim.min.js',
-        'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js',
-        'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js',
-  		'/js/app.js'
+      '/',
+      '/manifest.json',
+      'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css',
+      'https://code.jquery.com/jquery-3.2.1.slim.min.js',
+      'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js',
+      'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js',
+      '/js/app.js'
     ];
 
 // service worker installation
